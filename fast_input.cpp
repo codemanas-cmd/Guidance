848f16437e4b8f1f467693997cbe60309e93f258
codemanas-cmd <manasmishra2003lm@gmail.com>
2025-02-03 22:52:06 +0530

    add i/o

---
+++ fast_input.cpp
@@ -0,0 +1,14 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+int main () {
+    // try to remember this it helps in optimizing the 
+    // input/output time and makes code faster 
+    // have this at the top of main function
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    
+}
